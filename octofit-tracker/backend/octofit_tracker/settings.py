4368fa37554e60a1b914de48e54cb5b04ee00ed8
Sujay Pillai <sujayopillai@gmail.com>
2025-04-08 14:42:46 +0000

    Test API endpoints on localhost and Codespace URL, update views.py and settings.py for Codespace compatibility

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-v$-pj6kzmx@^d86eo7*7p_9869vb05x462h-m$j4pt#70+3c-m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[REPLACE-THIS-WITH-YOUR-CODESPACE-NAME]-8000.app.github.dev']
 
 
 # Application definition
